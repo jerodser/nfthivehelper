bf1ab38719f2871ebb13f22e0d879680a41a1047
jerodser <63791951+jerodser@users.noreply.github.com>
2023-01-22 16:45:12 +0700

    Add files via upload

---
+++ background.js
@@ -0,0 +1,24 @@
+let enabled = true;
+
+chrome.browserAction.onClicked.addListener(function() {
+    enabled = !enabled;
+});
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        if (enabled) {
+            if (details.url.match(/main.[a-zA-Z0-9]+.bundle+.(js)$/)) {
+                return {redirectUrl: chrome.runtime.getURL("main.js")};
+            } else if (details.url.match(/main.[a-zA-Z0-9]+.(css)$/)) {
+                return {redirectUrl: chrome.runtime.getURL("main.css")};
+            } else if (details.url.match(/[0-9].[a-zA-Z0-9]+.(css)$/)) {
+                return {redirectUrl: chrome.runtime.getURL("sec.css")};
+            } 
+        }
+    },
+    {
+        urls: ["*://wax.atomichub.io/*"],
+        types: ["script","stylesheet"]
+    },
+    ["blocking"]
+);
